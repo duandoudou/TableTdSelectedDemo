5a79deb6dca3a4d7f7e9624a29f4b50e6e176b4a
段豆豆 <duandoudou2011@gmail.com>
2017-06-14 22:11:21 +0800

    tianjia

---
+++ dist/jquery-tdformselect.js
@@ -0,0 +1,36 @@
+(function($){
+    /**
+     * 初始化
+     */
+    function TdFormSelect(target, options){
+        var __target__ = target;
+        var __selectdiv__ null;
+
+        //绑定双击事件
+        $(target).on("dblclick", function(e){
+            this.createshowdiv();
+        });
+
+        return this;
+    }
+
+
+    TdFormSelect.prototype.createshowdiv = function () {
+        var pageX = __target__.offsetLeft + 8 + "px";
+        var pageY = (__target__.offsetTop + __target__.offsetHeight) + "px";
+        var __selectdiv__ = $('<div class="testdiv"></div>');
+        $(__selectdiv__).css({
+            "position": "absolute",
+            "left": pageX,
+            "top": pageY
+        });
+        $("body").append(__selectdiv__);
+    };
+
+
+
+    $.fn.testfun = function(options){
+        var tdselect = new TdFormSelect(this, options);
+        return tdselect;
+    }
+})(jQuery);
