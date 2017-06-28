240f22241701a6dfb402c90ee8b7d0b804d356ba
duandoudou <duandoudou2011@gmail.com>
2017-06-28 17:51:24 +0800

    修改，完成Demo0.1

--- dist/jquery-tdformselect.js
+++ dist/jquery-tdformselect.js
@@ -1,3 +1,5 @@
+//# sourceURL=jquery-tdformselect.js
+
 (function($){
     /**
      * 初始化
@@ -5,8 +7,12 @@
     function TdFormSelect(target, options){
         this.__target__ = target;
         this.__selectdiv__ = null;
+        this.__options = {};
 
-        this.changeTdToInput();
+        this.__options.onclosefun = options.onclosefun ? options.onclosefun : null;
+        this.__options.tdallownull = options.tdallownull ? options.onclosefun : false;
+        this.__options.onitemcheck = options.onitemcheck ? options.onitemcheck : null;
+
         this.createshowdiv();
         this.bindClickEvent();
         this.bindCheckboxClick();
@@ -18,31 +24,51 @@
         var __this = this.__target__;
         //构建DIV
         var offset = $(__this).offset();
-        var pageX = offset.left + 8 + "px";
+        var pageX = offset.left + "px";
         var pageY = (offset.top + __this.outerHeight()) + "px";
+        var width = 300;
+        if(__this.outerWidth() < width){
+            width = __this.outerWidth();
+        }
         var __selectdiv__ = $('<div class="td-select-form"></div>');
         $(__selectdiv__).css({
             "position": "absolute",
             "left": pageX,
             "top": pageY,
-            "width": "300px",
-            "height": "400px",
-            "background-color": "#ff7400"
+            "width": width+"px",
+            "padding": "10px",
+            "z-index": "1699",
+            "background-color": "#fff",
+            "border": "1px solid #e4eaec",
+            "border": "1px solid rgba(204, 213, 219, .8)",
+            "border-radius": "4px",
+            "-webkit-box-shadow": "0 2px 6px rgba(0, 0, 0, .05)",
+            "        box-shadow": "0 2px 6px rgba(0, 0, 0, .05)",
+            "-webkit-background-clip": "padding-box",
+            "        background-clip": "padding-box"
         });
 
         //添加数据
         var data = $(__this).attr("data-select");
+        var value = $(__this).html();
         if(!data){
             return;
         }
         var datalist = data.split(",");
         for(var i=0;i<datalist.length;i++){
             var currval = datalist[i];
-            var div = $('<div class="select-row"></div>')
-            var checkbox = $('<input type="checkbox" select-val="'+currval+'" />');
-            var span = $('<span>'+currval+'</span>');
+            var div = $('<div class="select-row"></div>');
 
-            $(div).append(checkbox).append(span);
+            var select = "";
+            if(value && value.indexOf(currval) != -1){
+                select = "checked";
+            }
+
+            var checkbox_custom = $('<div class="checkbox-custom checkbox-primary control-label"></div>');
+            var input_label = $('<input type="checkbox" select-val="'+currval+'" '+select+'/><label for="inputChecked">'+currval+'</label>');
+
+            $(checkbox_custom).append(input_label);
+            $(div).append(checkbox_custom);
             $(__selectdiv__).append(div);
         }
         $("body").append(__selectdiv__);
@@ -51,25 +77,30 @@
 
     TdFormSelect.prototype.bindClickEvent = function(){
         var __td = this.__target__;
+        var __this = this;
         var selectdiv = this.__selectdiv__;
         //事件绑定
         $(selectdiv).on('click', function(e){
             e.stopPropagation();
         });
         //添加全局事件，监听是否去掉窗口
-        $(document).on('click', function(e){
-            var input = $(__td).find("input");
-            $(__td).html($(input).val());
-            $(__td).attr("title", $(input).val());
-            $(selectdiv).remove();
+        $(document).on('click', function(){
+            if(__this){
+                var closefunction = __this.__options.onclosefun;
+                if(closefunction){
+                    closefunction(__td, __this);
+                }
+                $(selectdiv).remove();
+                __this = null;
+            }
         });
-    }
+    };
 
     TdFormSelect.prototype.bindCheckboxClick = function(){
 
         var __td = this.__target__;
         var selectdiv = this.__selectdiv__;
-
+        var __this = this;
         $(selectdiv).on("click", "input[type='checkbox']", function(){
             var checkboxlist = $(selectdiv).find('input[type="checkbox"]');
             var value = "";
@@ -80,18 +111,29 @@
                     value += $(currbox).attr("select-val");
                 }
             }
-            var input = $(__td).find('input');
-            $(input).val(value);
-            $(input).attr("title", value);
-        });
-    }
 
-    TdFormSelect.prototype.changeTdToInput = function(){
-        var __this = this.__target__;
-        var value = $(__this).html();
-        var input = $('<input type="text" disabled value="'+value+'" />');
-        $(__this).html(input);
-    }
+            var returnFlag = true;
+
+            //设置不能为空
+            var allownull = __this.__options.tdallownull;
+            if(!allownull && !value){
+                value = $(this).attr("select-val");
+                returnFlag = false;
+            }
+
+            //设置回调
+            var itemclickfun = __this.__options.onitemcheck;
+
+            if(itemclickfun){
+                itemclickfun(value, __td, __this);
+            }
+
+            $(__td).html(value);
+            $(__td).attr("title", value);
+
+            return returnFlag;
+        });
+    };
 
     $.fn.tdFormSelect = function(options){
         var tdselect = new TdFormSelect(this, options);
